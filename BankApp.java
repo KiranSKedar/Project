89e3ba62769632d4363bfb9fb20060df9c997560
KiranSKedar <60933562+KiranSKedar@users.noreply.github.com>
2024-01-26 22:20:51 +0530

    Add files via upload

---
+++ BankApp.java
@@ -0,0 +1,15 @@
+import in.smartprogramming.icici.loan.carloan.CarLoan;
+import in.smartprogramming.icici.loan.homeloan.HomeLoan;
+
+
+class BankApp{
+	public static void main(String args[])
+	{
+		CarLoan c1=new CarLoan();
+		c1.getCarLoan();
+
+		HomeLoan h=new HomeLoan();
+		h.getHomeLoan();
+	}
+
+}
